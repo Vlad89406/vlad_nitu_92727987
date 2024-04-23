c52765d79a4202c1441ad3721f4e1b5d5c062f8a
Vlad Nitu <vladn@umich.edu>
2024-04-23 03:12:27 +0000

    django

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import react from "react"
+import React from "react"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {

e80f1d5d65a32e0fbb0971aa0e1edc818c009687
Vlad Nitu <vladn@umich.edu>
2024-04-23 03:39:11 +0000

    file restructure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "22375bc3d9cc0b39"; }
+/******/ 		__webpack_require__.h = function() { return "d02e7f13fa45904d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
